885ec1ab36a70b53338748613fd31c3140a5005a
Furkanblbl <furkanbulbul2222@gmail.com>
2025-03-08 14:00:29 +0300

    Red circle detection

--- extraction.py
+++ extraction.py
@@ -1,43 +1,40 @@
 import cv2
 import numpy as np
 
-img = cv2.imread('images/shapes.png')
+img = cv2.imread('images/underwater.jpg')
 img = cv2.resize(img,(480,480))
 
 hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-color_ranges = {
-    "red": [(0, 120, 70), (10, 255, 255)],
-    "blue": [(90, 50, 50), (130, 255, 255)],
-    "green": [(35, 50, 50), (85, 255, 255)]
-}
+lower_red1 = np.array([0, 120, 70])
+upper_red1 = np.array([10, 255, 255])
+lower_red2 = np.array([170, 120, 70])
+upper_red2 = np.array([180, 255, 255])
 
-def detect_shape(contour):
-    approx = cv2.approxPolyDP(contour, 0.02 * cv2.arcLength(contour, True), True)
-    sides = len(approx)
-    if sides == 3:
-        return "triangle"
-    elif sides == 4:
-        return "square"
-    else:
-        return "circle"
+# Kırmızı renkleri maskeleme
+mask1 = cv2.inRange(hsv, lower_red1, upper_red1)
+mask2 = cv2.inRange(hsv, lower_red2, upper_red2)
 
-selected_color = "red"
-selected_shape = "triangle"
+mask = mask1 + mask2
 
-lower, upper = color_ranges[selected_color]
-mask = cv2.inRange(hsv, np.array(lower), np.array(upper))
+# Apply erode and dilate
+kernel = np.ones((5, 5), np.uint8)
+mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
+mask = cv2.morphologyEx(mask, cv2.MORPH_DILATE, kernel)
 
-# Kenar tespiti
+# Find contours
 contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-# Belirlenen objeleri bul
 for contour in contours:
-    shape = detect_shape(contour)
-    if shape == selected_shape:
-        # Seçilen objeye dikdörtgen çiz
-        x, y, w, h = cv2.boundingRect(contour)
-        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    # Finding the smallest circle enclosed by the contour
+    (x, y), radius = cv2.minEnclosingCircle(contour)
+    print("x: ",x,"  y: ",y,"  rad: ",radius)
+    center = (int(x), int(y))
+    radius = int(radius)
+
+    # Checking if there is a circle
+    if radius > 10:
+        cv2.circle(img, center, radius, (0, 255, 0), 3)
 
 
 cv2.imshow("shapes", img)
